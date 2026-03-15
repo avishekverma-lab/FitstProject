3be2623d4f8e85a4672ae60c0c8c21a757b5c408
Avishek Verma <avishek.verma@gmail.com>
2026-03-15 12:01:44 +0530

    updated python code

--- Credentials_with_log_file.py
+++ Credentials_with_log_file.py
@@ -1,13 +1,19 @@
+import datetime
+
 username = input("Enter your username: ")
 password = input("Enter your password: ")
 
 print(f"Username: {username}")
 print(f"Password: {password}")
 
+# Get current date and time
+current_time = datetime.datetime.now()
+
 # Write to a log file
-with open('credentials.log', 'a') as f:
-    f.write(f"------------------------\n")
+with open('E:\\git\\FitstProject\\credentials.log', 'a') as f:
+    f.write(f"======================== {current_time} ========================:\n")
     f.write(f"Username: {username}\n")
     f.write(f"Password: {password}\n")
+    f.write("\n") # Add a newline for better readability between entries
 
-print("E:\git\FitstProject\Credentials saved to credentials.log")
+print("Credentials and timestamp saved to credentials.log")2234
